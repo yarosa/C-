5faffa9b6ede763b3c1d64fa778c1f9d9c333b1d
yarosa <yarosa@gmail.com>
2020-08-12 23:26:42 +0300

    files added

---
+++ what.cpp
@@ -0,0 +1,12 @@
+// "what.cpp" - program
+
+#include <iostream>
+
+int main()
+{
+	// using namespace std
+	// using namespace
+	// cout << "What's up, Doc!\n" << endl
+	std::cout << "What's up, Doc!" << std::endl;
+	return 0;
+}
